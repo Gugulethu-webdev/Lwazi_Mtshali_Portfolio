275cfe5466dcbfc8bc9198eaeca71fed62d8ffc7
Gugulethu-webdev <mtshaliamo@gmail.com>
2020-03-12 11:29:45 +0200

    added css grid

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,10 +22,10 @@ function toggleMenu(){
         // set menu state
         showMenu = true;
     }else{
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
         navItems.forEach(item=>item.classList.remove('show'));
 
         // set menu state
